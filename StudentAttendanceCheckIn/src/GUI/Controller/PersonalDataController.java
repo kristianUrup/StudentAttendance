d941b0dd86665c6346c86449a6018863e7172a5a
kristianUrup <43743914+kristianUrup@users.noreply.github.com>
2019-02-06 12:21:00 +0100

    lavet fxml filer

--- StudentAttendanceCheckIn/src/GUI/Controller/PersonalDataController.java
+++ StudentAttendanceCheckIn/src/GUI/Controller/PersonalDataController.java
@@ -5,13 +5,18 @@
  */
 package GUI.Controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -54,8 +59,19 @@ public class PersonalDataController implements Initializable
     }    
 
     @FXML
-    private void handleCheckIn(ActionEvent event)
+    private void handleCheckIn(ActionEvent event) throws IOException
     {
+        int cprNr = Integer.parseInt(txtCprNr.getText());
+        if(cprNr == 1)
+        {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/View/Student/StudentScreenController"));
+            Parent root = (Parent) loader.load();
+            
+            GUI.Controller.Student.StudentScreenController sccontroller = loader.getController();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+        }
     }
 
     @FXML
