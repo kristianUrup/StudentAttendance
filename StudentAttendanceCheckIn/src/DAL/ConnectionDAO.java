d2f30d8f69652f18856b95074d5e9cd660d8b060
Schweizeren <j_wannerhenriksen@hotmail.com>
2019-03-15 10:00:45 +0100

    ConnectionDAO

---
+++ StudentAttendanceCheckIn/src/DAL/ConnectionDAO.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAL;
+
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+import java.sql.Connection;
+
+/**
+ *
+ * @author Schweizeren
+ */
+public class ConnectionDAO
+{
+    private static final String SERVER_NAME = "10.176.111.31";
+    private static final String DATABASE_NAME = "PrivateMovies";
+    private static final String USER = "CS2018A_14";
+    private static final String PASSWORD = "CS2018A_14";
+    SQLServerDataSource ds;
+    
+    public ConnectionDAO()
+    {
+        ds = new SQLServerDataSource();
+        ds.setServerName(SERVER_NAME);
+        ds.setDatabaseName(DATABASE_NAME);
+        ds.setUser(USER);
+        ds.setPassword(PASSWORD);
+    }
+    
+    public Connection getConnection() throws SQLServerException {
+        Connection con = ds.getConnection();
+        return con;
+    }
+}
